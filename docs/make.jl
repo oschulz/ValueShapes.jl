f22ba2f5bff66dd47cb58d69cc3d9d34aac5328b
Oliver Schulz <oschulz@mpp.mpg.de>
2019-03-04 15:53:03 +0100

    Update to Documenter.jl v0.21

--- docs/make.jl
+++ docs/make.jl
@@ -10,17 +10,19 @@ using ShapesOfVariables
 makedocs(
     sitename = "ShapesOfVariables",
     modules = [ShapesOfVariables],
-    format = :html,
+    format = Documenter.HTML(
+        prettyurls = !("local" in ARGS),
+        canonical = "https://oschulz.github.io/ShapesOfVariables.jl/stable/"
+    ),
     pages=[
         "Home" => "index.md",
         "API" => "api.md",
         "LICENSE" => "LICENSE.md",
     ],
     doctest = ("fixdoctests" in ARGS) ? :fix : true,
-    html_prettyurls = !("local" in ARGS),
-    html_canonical = "https://oschulz.github.io/ShapesOfVariables.jl/stable/",
 )
 
 deploydocs(
-    repo = "github.com/oschulz/ShapesOfVariables.jl.git"
+    repo = "github.com/oschulz/ShapesOfVariables.jl.git",
+    forcepush = true
 )
