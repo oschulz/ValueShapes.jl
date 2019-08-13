dd2ab575ee8b1b5c36d379c3178bfe3b8f42db0f
Oliver Schulz <oschulz@mpp.mpg.de>
2019-08-13 02:52:21 +0200

    Define array getindex and view with VariableDataAccessor

--- src/varshapes.jl
+++ src/varshapes.jl
@@ -16,52 +16,67 @@ VariableDataAccessor(shape::S, offset::Int) where {S<:AbstractValueShape} = Vari
 nonabstract_eltype(va::VariableDataAccessor) = nonabstract_eltype(va.shape)
 
 
+const ScalarAccessor{T} = VariableDataAccessor{ScalarShape{T}} where {T}
+const ArrayAccessor{T,N} = VariableDataAccessor{ArrayShape{T,N}} where {T,N}
+const ConstAccessor = VariableDataAccessor{ConstValueShape{T}} where {T}
+
+
 @inline function _view_range(idxs::AbstractUnitRange{<:Integer}, va::VariableDataAccessor)
     from = first(idxs) + va.offset
     to = from + va.len - 1
     from:to
 end
 
-
-const ScalarAccessor{T} = VariableDataAccessor{ScalarShape{T}} where {T}
-const ArrayAccessor{T,N} = VariableDataAccessor{ArrayShape{T,N}} where {T,N}
-const ConstAccessor = VariableDataAccessor{ConstValueShape{T}} where {T}
+@inline _view_idxs(idxs::AbstractUnitRange{<:Integer}, va::VariableDataAccessor) = _view_range(idxs, va)
+@inline _view_idxs(idxs::AbstractUnitRange{<:Integer}, va::ScalarAccessor) = first(idxs) + va.offset
 
 
 Base.@propagate_inbounds (va::ScalarAccessor)(data::AbstractVector) =
-    data[first(_view_range(axes(data, 1), va))]
+    data[_view_idxs(axes(data, 1), va)]
 
 Base.@propagate_inbounds (va::ArrayAccessor{T,1})(data::AbstractVector) where {T} =
-    view(data, _view_range(axes(data, 1), va))
+    view(data, _view_idxs(axes(data, 1), va))
 
 Base.@propagate_inbounds (va::ArrayAccessor{T,N})(data::AbstractVector) where {T,N} =
-    reshape(view(data, _view_range(axes(data, 1), va)), size(va.shape)...)
+    reshape(view(data, _view_idxs(axes(data, 1), va)), size(va.shape)...)
 
 @inline (va::ConstAccessor)(::AbstractVector) = va.shape.value
 
 
 Base.@propagate_inbounds function (va::ScalarAccessor)(data::AbstractVectorOfSimilarVectors)
     flat_data = flatview(data)
-    idxs = _view_range(axes(flat_data, 1), va)
-    view(flat_data, first(idxs), :)
+    idxs = _view_idxs(axes(flat_data, 1), va)
+    view(flat_data, idxs, :)
 end
 
 Base.@propagate_inbounds function (va::ArrayAccessor{T,1})(data::AbstractVectorOfSimilarVectors) where {T,N}
     flat_data = flatview(data)
-    idxs = _view_range(axes(flat_data, 1), va)
+    idxs = _view_idxs(axes(flat_data, 1), va)
     fpview = view(flat_data, idxs, :)
     VectorOfSimilarVectors(fpview)
 end
 
 Base.@propagate_inbounds function (va::ArrayAccessor{T,N})(data::AbstractVectorOfSimilarVectors) where {T,N}
     flat_data = flatview(data)
-    idxs = _view_range(axes(flat_data, 1), va)
+    idxs = _view_idxs(axes(flat_data, 1), va)
     fpview = view(flat_data, idxs, :)
     VectorOfSimilarArrays(reshape(fpview, size(va.shape)..., :))
 end
 
 @inline (va::ConstAccessor)(data::AbstractVectorOfSimilarVectors) =
     Fill(va.shape.value, size(data,1))
+
+
+Base.@propagate_inbounds Base.getindex(data::AbstractVector, va::VariableDataAccessor) = va(data)
+
+
+Base.@propagate_inbounds Base.view(data::AbstractVector, va::VariableDataAccessor) = va(data)
+Base.@propagate_inbounds Base.view(data::AbstractVector{<:AbstractVector}, va::VariableDataAccessor) = va(data)
+
+
+Base.@propagate_inbounds function Base.view(data::AbstractVector, va::ScalarAccessor)
+    view(data, _view_idxs(axes(data, 1), va))
+end
 
 
 @inline _varoffset_cumsum_impl(s, x, y, rest...) = (s, _varoffset_cumsum_impl(s+x, y, rest...)...)
