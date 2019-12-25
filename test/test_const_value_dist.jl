70c8a3573d17a69a6dbd7b41ce942f00e1b607f3
tldrma <6267370+tldrma@users.noreply.github.com>
2019-12-25 23:23:15 +0100

    First attempt at a simple series of tests for const_value_dist.jl

--- test/test_const_value_dist.jl
+++ test/test_const_value_dist.jl
@@ -7,5 +7,25 @@ using Distributions
 
 
 @testset "const_value_dist" begin
-    # ToDo: Add tests
+    cvd = ConstValueDist(42)
+    shape = varshape(cvd)
+
+    @test @inferred insupport(cvd, 42) == true
+    @test @inferred insupport(cvd, 41.999) == false
+    @test @inferred insupport(cvd, 42.999) == false
+    @test @inferred totalndof(shape) == 0
+    @test @inferred size(cvd) == ()
+    @test @inferred length(cvd) == 1
+    
+    @test @inferred minimum(cvd.value) == maximum(cvd.value)
+    
+    @test @inferred pdf(cvd, 42) == Inf
+    
+    @test @inferred cdf(cvd, 41.999) == 0
+    @test @inferred cdf(cvd, 42) == 1
+
+    @test @inferred mode(cvd) == 42
+
+    @test @inferred rand(cvd, 10) == fill(42, 10)
+
 end
