b091c2e62645657af6b3c6dac7d6724a5c3e4639
tldrma <6267370+tldrma@users.noreply.github.com>
2019-12-28 22:20:20 +0100

    Added parentheses to inferred macro used in test_const_value_dist.jl

--- test/test_const_value_dist.jl
+++ test/test_const_value_dist.jl
@@ -9,38 +9,37 @@ using Random
 
 @testset "const_value_dist" begin
     univariate_cvd = ConstValueDist(Int64(42))
-    v = [1, 2, 3]
-    v = broadcast(Int64, v)
+    v = broadcast(Int64, [1,2, 3])
     multivariate_cvd = ConstValueDist(v)
-    @test @inferred length(multivariate_cvd) == length(v)
+    @test @inferred(length(multivariate_cvd) == length(v))
     
     shape = varshape(univariate_cvd)
 
-    @test @inferred insupport(univariate_cvd, 42) == true
-    @test @inferred insupport(univariate_cvd, 41.999) == false
-    @test @inferred insupport(univariate_cvd, 42.999) == false
-    @test @inferred totalndof(shape) == 0
-    @test @inferred size(univariate_cvd) == ()
-    @test @inferred length(univariate_cvd) == 1
+    @test @inferred(insupport(univariate_cvd, 42) == true)
+    @test @inferred(insupport(univariate_cvd, 41.999) == false)
+    @test @inferred(insupport(univariate_cvd, 42.999) == false)
+    @test @inferred(totalndof(shape) == 0)
+    @test @inferred(size(univariate_cvd) == ())
+    @test @inferred(length(univariate_cvd) == 1)
     
-    @test @inferred minimum(univariate_cvd) == 42
-    @test @inferred maximum(univariate_cvd) == 42
-    @test @inferred maximum(pdf(univariate_cvd, -50:50)) == Inf
+    @test @inferred(minimum(univariate_cvd) == 42)
+    @test @inferred(maximum(univariate_cvd) == 42)
+    @test @inferred(maximum(pdf(univariate_cvd, -50:50)) == Inf)
     
-    @test @inferred pdf(univariate_cvd, 42) == Inf
+    @test @inferred(pdf(univariate_cvd, 42) == Inf)
     
-    @test @inferred cdf(univariate_cvd, 41.999) == 0
-    @test @inferred cdf(univariate_cvd, 42) == 1
-    @test @inferred cdf(univariate_cvd, Inf) == 1
+    @test @inferred(cdf(univariate_cvd, 41.999) == 0)
+    @test @inferred(cdf(univariate_cvd, 42) == 1)
+    @test @inferred(cdf(univariate_cvd, Inf) == 1)
 
-    @test @inferred mode(univariate_cvd) == 42
+    @test @inferred(mode(univariate_cvd) == 42)
 
-    @test @inferred rand(univariate_cvd, 10) == fill(42, 10)
+    @test @inferred(rand(univariate_cvd, 10) == fill(42, 10))
 
-    @test @inferred logpdf(univariate_cvd, -1) == -Inf 
-    @test @inferred logpdf(univariate_cvd, 41.999) == -Inf 
-    @test @inferred logpdf(univariate_cvd, 42) == Inf 
-    @test @inferred logpdf(univariate_cvd, 43.999) == -Inf 
+    @test @inferred(logpdf(univariate_cvd, -1) == -Inf)
+    @test @inferred(logpdf(univariate_cvd, 41.999) == -Inf)
+    @test @inferred(logpdf(univariate_cvd, 42) == Inf)
+    @test @inferred(logpdf(univariate_cvd, 43.999) == -Inf )
     
-    @test @inferred eltype(univariate_cvd) == Int64
+    @test @inferred(eltype(univariate_cvd) == Int64)
 end
