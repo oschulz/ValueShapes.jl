829484d32a7c847281699e276bef78edba7c50cb
Scott Hayashi <Scott.Hayashi@tum.de>
2020-01-19 21:17:36 +0100

    Initial commit with some minor tests. codecov .yaml changed

--- test/test_value_shape.jl
+++ test/test_value_shape.jl
@@ -17,5 +17,19 @@ import TypedTables
 
         @test @inferred(elshape(Complex(1.0, 2.0))) == ScalarShape{Complex{Float64}}()
         @test @inferred(elshape([[3, 5], [3, 2]])) == ArrayShape{Int,1}((2,))
+        
+        arrshape = ArrayShape{Real, 2}((2,3))
+        @test @inferred(length(Vector{Real}(undef, arrshape))) == 6
+        @test @inferred(size(Vector{Real}(undef, arrshape))) == (6,)
+        
+        data = [1;2;3;4;7;8;9]
+        scalarshape = ScalarShape{Real}()
+        ntshape = NamedTupleShape(a=arrshape, b=scalarshape)
+        shapedasnt = ntshape(data)       
+        @test stripscalar(Ref(shapedasnt)) == Ref(shapedasnt)[]
+
+        @test ValueShapes._checkcompat_inner
+        
+ 
     end
 end
