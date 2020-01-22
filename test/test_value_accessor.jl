3a38ed1cfec168d739f854a0bfa2bdb4ea87bbd0
Scott H <scott.hayashi@tum.de>
2020-01-22 14:02:40 +0100

    Add tests for ValueAccessor

--- test/test_value_accessor.jl
+++ test/test_value_accessor.jl
@@ -8,4 +8,13 @@ using Test
     @test @inferred(valshape(acc)) == ArrayShape{Real,2}((2, 3))
     data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @test @inferred(data[acc]) == [3 5 7; 4 6 8]
+
+    @test_throws ArgumentError Broadcast.broadcastable(acc)
+
+    @test @inferred(size(acc)) == getproperty(getproperty(acc, :shape), :dims)
+
+    @test @inferred(length(acc)) == length(getproperty(acc, :shape))
+
+    @test @inferred(ValueShapes.default_unshaped_eltype(acc)) == @inferred(ValueShapes.default_unshaped_eltype(getproperty(acc, :shape)))
+    
 end
