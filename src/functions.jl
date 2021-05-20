3ad32c7fb359b426296d62c4b02eb1077499db6d
Oliver Schulz <oschulz@mpp.mpg.de>
2021-05-20 13:27:29 +0200

    Add unshaped for functions

--- src/functions.jl
+++ src/functions.jl
@@ -33,6 +33,48 @@ vardof(f::Function) = totalndof(varshape(f))
 
 
 
+@inline _maybe_shaped(x::Any, vs::Nothing) = x
+Base.@propagate_inbounds _maybe_shaped(x::Any, vs::AbstractValueShape) = stripscalar(vs(x))
+
+@inline _maybe_unshaped(x::Any, vs::Nothing) = x
+Base.@propagate_inbounds _maybe_unshaped(x::Any, vs::AbstractValueShape) = unshaped(x, vs)
+
+
+struct UnshapedFunction{F<:Function,IS<:Union{Nothing,AbstractValueShape},OS<:Union{Nothing,AbstractValueShape}} <: Function
+    orig_f::F
+    orig_varshape::IS
+    orig_valshape::OS
+end
+
+"""
+    unshaped(
+        f::Function,
+        orig_varshape::Union{Nothing,AbstractValueShape},
+        orig_valshape::Union{Nothing,AbstractValueShape} = nothing
+    )
+
+Return a function that
+    * Shapes it's input from a flat vector of `Real` using `orig_varshape`, if not `nothing`.
+    * Calls `f` with the (optionally) unshaped input.
+    * Unshapes the result to a flat vector of `Real` using `orig_valshape`, if not `nothing`.
+"""
+function unshaped(f::Function, orig_varshape::Union{Nothing,AbstractValueShape}, orig_valshape::Union{Nothing,AbstractValueShape} = nothing)
+    UnshapedFunction(f, orig_varshape, orig_valshape)
+end
+
+varshape(uf::UnshapedFunction) = uf.varshape
+
+Base.@propagate_inbounds function (uf::UnshapedFunction)(x)
+    orig_x =_maybe_shaped(x, uf.orig_varshape)
+    orig_y = uf.orig_f(orig_x) 
+    _maybe_unshaped(orig_y, uf.orig_valshape)
+end
+
+
+
+
+
+# ToDo: Deprecate/remove:
 struct FuncWithVarShape{F<:Function,VS<:AbstractValueShape} <: Function
     f::F
     varshape::VS
@@ -48,5 +90,6 @@ Base.@propagate_inbounds function (fws::FuncWithVarShape)(x::AbstractVector{<:Re
     fws.f(x_shaped)
 end
 
+# ToDo: Deprecate/remove:
 import Base.>>
 @inline >>(varshape::AbstractValueShape, f::Function) = FuncWithVarShape(f, varshape)
--- test/test_functions.jl
+++ test/test_functions.jl
@@ -3,23 +3,48 @@
 using ValueShapes
 using Test
 
+using LinearAlgebra
+
 
 @testset "functions" begin
-    shape = NamedTupleShape(
+    input_shape = NamedTupleShape(
         a = ArrayShape{Real}(3,2),
         b = ArrayShape{Real}(2)
     )
 
-    x_flat = rand(@inferred totalndof(shape))
-    x = @inferred stripscalar(shape(x_flat))
+    output_shape = NamedTupleShape(
+        x = ScalarShape{Real}(),
+        y = ArrayShape{Real}(3)
+    )
 
-    f(x) = sum(x.a * x.b)
 
-    @test @inferred(shape >> f) isa ValueShapes.FuncWithVarShape
-    @test @inferred(varshape((shape >> f))) == shape
-    @test @inferred(vardof((shape >> f))) == totalndof(shape)
+    @testset "UnshapedFunction" begin
+        f(x) = (x = norm(x.a)^2 + norm(x.b)^2, y = vec(sum(x.a, dims = 2)))
+        ux = [2, 7, 4, 6, 8, 5, 3, 1]
+        x = input_shape(ux)[]
+        y = f(x)
+        uy = unshaped(y, output_shape)
+        @test @inferred(unshaped(f, input_shape)(ux)) == y
+        @test @inferred(unshaped(f, input_shape, nothing)(ux)) == y
+        @test @inferred(unshaped(f, input_shape, output_shape)(ux)) == uy
+        @test @inferred(unshaped(f, nothing, output_shape)(x)) == uy
+        @test @inferred(unshaped(f, nothing)(x)) == y
+        @test @inferred(unshaped(f, nothing, nothing)(x)) == y
+    end
 
-    fws = shape >> f
-    @test f(x) == @inferred(fws(x))
-    @test f(x) == @inferred(fws(x_flat))
+
+    @testset "FuncWithVarShape" begin
+        x_flat = rand(@inferred totalndof(input_shape))
+        x = @inferred stripscalar(input_shape(x_flat))
+
+        g(x) = sum(x.a * x.b)
+
+        @test @inferred(input_shape >> g) isa ValueShapes.FuncWithVarShape
+        @test @inferred(varshape((input_shape >> g))) == input_shape
+        @test @inferred(vardof((input_shape >> g))) == totalndof(input_shape)
+
+        fws = input_shape >> g
+        @test g(x) == @inferred(fws(x))
+        @test g(x) == @inferred(fws(x_flat))
+    end
 end
