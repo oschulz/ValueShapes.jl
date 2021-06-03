009f3d00ef2396c2e4fccc36c676da126c44ca4b
sthayashi <scott.hayashi@tum.de>
2021-06-03 07:34:51 +0200

    Adds tests for ConstValueDist constructed using a NamedTuple (#44)

--- test/test_const_value_dist.jl
+++ test/test_const_value_dist.jl
@@ -77,4 +77,23 @@ using Random
     @test @inferred(mode(univariate_cvd)) == 42
 
     @test @inferred(eltype(univariate_cvd)) == Int64
+    
+    μ1, μ2 = rand(2)
+    cvd_from_named_tuple = @inferred(ConstValueDist((a=μ1, b=μ2)))
+
+    @test @inferred(log(@inferred(pdf(cvd_from_named_tuple, (a=μ1, b=μ2))))) == @inferred(logpdf(cvd_from_named_tuple, (a=μ1, b=μ2))) == Inf
+
+    @test @inferred(insupport(cvd_from_named_tuple, (a=μ1, b=μ2))) == true
+    @test @inferred(insupport(cvd_from_named_tuple, (a=μ1+eps(μ1), b=μ2+eps(μ2)))) == false
+    @test @inferred(insupport(cvd_from_named_tuple, (a=μ1+eps(μ1), b=μ2))) == false
+    @test @inferred(insupport(cvd_from_named_tuple, (a=μ1, b=μ2+eps(μ2)))) == false
+
+    n_samples = 100
+    samples = @inferred(rand(cvd_from_named_tuple, n_samples))
+    emptied_samples = @inferred(similar(samples))
+
+    @test emptied_samples != samples
+    @test samples == @inferred(fill((a=μ1, b=μ2), n_samples))
+    @test @inferred(rand!(cvd_from_named_tuple, emptied_samples)) == samples
+    @test samples == emptied_samples
 end
