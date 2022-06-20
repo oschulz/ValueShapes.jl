fe9bb431cb3d400e8b2829573ec6022f89f344d6
Oliver Schulz <oschulz@mpp.mpg.de>
2022-06-19 22:16:10 -0400

    STASH

---
+++ src/vs_vartransform.jl
@@ -0,0 +1,16 @@
+# This file is a part of ValueShapes.jl, licensed under the MIT License (MIT).
+
+
+function resultshape(f::MeasureBase.VarTransformation, vs::AbstractValueShape)
+    @argcheck vs <= varshape(f.μ)
+    return varshape(trafo.ν)
+end
+
+
+function Base.Broadcast.broadcasted(
+    trafo::VarTransformation,
+    v_src::Union{ArrayOfSimilarVectors{<:Real},ShapedAsNTArray}
+)
+    return broadcast_trafo(trafo, v_src)
+end
+
