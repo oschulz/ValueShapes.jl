1da3305a41176b9d3646a224898545d2cafa61ab
Oliver Schulz <oschulz@mpp.mpg.de>
2022-06-20 16:03:52 -0400

    STASH testutils

--- test/testutils.jl
+++ test/testutils.jl
@@ -3,33 +3,40 @@
 using ValueShapes
 using Test
 
+using LinearAlgebra
+using InverseFunctions, ChangesOfVariables
 import MeasureBase
 import ForwardDiff
 
 function test_transport(ν, μ)
     supertype(x::Real) = Real
     supertype(x::AbstractArray{<:Real,N}) where N = AbstractArray{<:Real,N}
+    supertype(x::NamedTuple{names}) where names = NamedTuple{names}
+
+    approxcmp(a, b) = a ≈ b
+    approxcmp(a::Tuple, b::Tuple) = all(map(approxcmp, a, b))
+    approxcmp(a::NamedTuple{names}, b::NamedTuple{names}) where names = all(map(approxcmp, a, b))
 
     @testset "transport from $(nameof(typeof(ν))) to $(nameof(typeof(μ)))" begin
         x = rand(μ)
-        @test !(@inferred(MeasureBase.transport_to(ν, μ)(x)) isa NoTransport)
+        @test !(@inferred(MeasureBase.transport_to(ν, μ)(x)) isa MeasureBase.NoTransport)
         f = MeasureBase.transport_to(ν, μ)
         y = f(x)
-        @test @inferred(inverse(f)(y)) ≈ x
+        @test approxcmp(@inferred(inverse(f)(y)), x)
         @test @inferred(with_logabsdet_jacobian(f, x)) isa Tuple{supertype(y),Real}
         @test @inferred(with_logabsdet_jacobian(inverse(f), y)) isa Tuple{supertype(x),Real}
         y2, ladj_fwd = with_logabsdet_jacobian(f, x)
         x2, ladj_inv = with_logabsdet_jacobian(inverse(f), y)
         
-        @test x ≈ x2
-        @test y ≈ y2
+        @test approxcmp(x, x2)
+        @test approxcmp(y, y2)
         @test ladj_fwd ≈ - ladj_inv
         @test ladj_fwd ≈ logdensityof(μ, x) - logdensityof(ν, y)
 
         vs_μ = varshape(μ)
         vs_ν = varshape(ν)
-        ladj_fwd_ad = logabsdet(ForwardDiff.jacobian(inverse(vs_ν) ∘ f ∘ vs_μ, inverse(vs_μ)(x)))
-        ladj_inv_ad = logabsdet(ForwardDiff.jacobian(inverse(vs_μ) ∘ inverse(f) ∘ vs_ν, inverse(vs_ν)(y)))
+        ladj_fwd_ad = logabsdet(ForwardDiff.jacobian(inverse(vs_ν) ∘ f ∘ vs_μ, inverse(vs_μ)(x)))[1]
+        ladj_inv_ad = logabsdet(ForwardDiff.jacobian(inverse(vs_μ) ∘ inverse(f) ∘ vs_ν, inverse(vs_ν)(y)))[1]
         @test ladj_fwd ≈ ladj_fwd_ad
         @test ladj_inv ≈ ladj_inv_ad
     end
